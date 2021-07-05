2255eecddd8335be067fa384f4399b5679dff588
Linas Nikiperavičius <linas@linasdev.com>
2021-07-05 22:47:10 +0300

    Add a global heap allocator

--- src/ross-programmer/bin.rs
+++ src/ross-programmer/bin.rs
@@ -1,12 +1,20 @@
 #![no_std]
 #![no_main]
+#![feature(alloc_error_handler)]
 
 use panic_semihosting as _;
 
+use alloc_cortex_m::CortexMHeap;
+use core::alloc::Layout;
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use stm32f1xx_hal::prelude::*;
 use stm32f1xx_hal::pac::Peripherals;
+
+const HEAP_SIZE: usize = 4096;
+
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 
 #[entry]
 fn main() -> ! {
@@ -23,7 +31,19 @@ fn main() -> ! {
         .pclk2(72.mhz())
         .freeze(&mut flash.acr);
 
+    allocate_heap();
+
     loop {
         nop();
     }
+}
+
+fn allocate_heap() {
+    let start = cortex_m_rt::heap_start() as usize;
+    unsafe { ALLOCATOR.init(start, HEAP_SIZE) }
+}
+
+#[alloc_error_handler]
+fn oom(_: Layout) -> ! {
+    loop {}
 }
